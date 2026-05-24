8ce691a9f9c37d0c73c85b63e87a1289245a7690
Samuel Pulfer <samuel.pulfer@immeditech.ch>
2026-05-24 22:19:11 +0000

    list: expose firewall category as queryable target
    Allows `oxlorg.opnsense.list` with `target: category` to enumerate
    all OPNsense firewall categories. Useful for drift detection
    workflows that need to know which categories exist before tagging.

--- plugins/modules/list.py
+++ plugins/modules/list.py
@@ -50,7 +50,7 @@ TARGETS = [
     'haproxy_maintenance', 'haproxy_cpu', 'haproxy_user', 'haproxy_group', 'haproxy_acl', 'haproxy_action',
     'haproxy_lua', 'haproxy_fcgi', 'haproxy_errorfile', 'haproxy_mailer', 'haproxy_mapfile',
     'haproxy_resolver', 'haproxy_backend', 'haproxy_frontend', 'haproxy_healthcheck', 'haproxy_server',
-    'wazuh_agent', 'nut',
+    'wazuh_agent', 'nut', 'category',
 ]
 
 
@@ -680,6 +680,10 @@ def run_module():
         elif target == 'nut':
             from ansible_collections.oxlorg.opnsense.plugins.module_utils.main.nut import \
                 Nut as Target_Obj
+
+        elif target == 'category':
+            from ansible_collections.oxlorg.opnsense.plugins.module_utils.main.category import \
+                Category as Target_Obj
 
     except AttributeError:
         module_dependency_error()
